aae2022e4de17382cd1eec7c6def830e0b73c869
Rafi <smrafi120@gmail.com>
2023-05-14 20:29:36 +0600

    done

--- userDefinedDataStructure/LinearSearce.cpp
+++ userDefinedDataStructure/LinearSearce.cpp
@@ -3,30 +3,26 @@
 using namespace std;
 
 int main(){
-    int array_size,i;
+    int array_size;
     cin>>array_size;
-
-    int arr[array_size];
-
-    for(int i=0; i<array_size; i++){
-        cin>>arr[i];
-    }
-
-    int search_num;
-    cin>>search_num;
-    int found;
-    for(int i=0;i<array_size;i++){
-        if(search_num == arr[i]){
-           cout<<search_num;
-        }
-    }
-
-    if(i<=array_size){
-    	cout<<"Not found!\n";
-	}
-
-
-
     
+    int arr[array_size];
+    
+    for(int i=0;i<array_size;i++){
+    	cin>>arr[i];
+	}
+	int searching_item;
+	cin>>searching_item;
+	int counter =0;
+	for(int i=0;i<array_size;i++){
+		if(searching_item == arr[i]){
+			cout<<searching_item<<" is found at index "<<i<<" "<<endl;
+		}else{
+			counter++;
+		}
+	}
+	if(counter == array_size){
+		cout<<searching_item<<" is not found!\n";
+	}
     return 0;
 }
