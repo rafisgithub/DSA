9e9b0b8d991a6cd3dc24ad8b5f5ffe325e2df35e
Rafi Ahmed <smrafi120@gmail>
2023-09-09 18:14:04 +0600

    done

--- BinarySearce.cpp
+++ BinarySearce.cpp
@@ -1,17 +1,37 @@
 #include<iostream>
-
 using namespace std;
 
-int main() {
+int main(){
     int array_size;
-    int i;
+    cout<<"Enter array size:";
     cin>>array_size;
+    long long int arr[array_size];
 
-    int arr[array_size];
-    for(i=0;i<array_size;i++) {
+    cout<<"Enter array elements:";
+    for(int i = 0;i<array_size;i++){
         cin>>arr[i];
     }
+    int mid;
+    int l = 0;
+    int r = array_size-1;
+    int searchingElement;
+    cout<<"Enter searching element:";
+    cin>>searchingElement;
 
-    int l = 
+    while(l<=r){
+        mid = (l+r)/2;
+        if(searchingElement == arr[mid]){
+            cout<<"Elements is found at index "<<mid<<endl;
+            break;
+        }else if(searchingElement<arr[mid]){
+            r = mid - 1;
+        }else{
+            l = mid+1;
+        }
+    }
+
+    if(l>r){
+        cout<<"Elements is not found!"<<endl;
+    }
     return 0;
 }
